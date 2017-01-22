fd6336b9e5abfd0e351c6768d0668d1dfe4da2b4
XDEX <rc840327@gmail.com>
2017-01-22 15:39:29 +0800

    1. Finish Map Practice

--- map/map/ViewController.swift
+++ map/map/ViewController.swift
@@ -12,7 +12,7 @@ import MapKit
 class ViewController: UIViewController {
     
     @IBOutlet var mapView: MKMapView!
-    
+    var mode : Int = 0
     let lm = LocationManager.sharedInstance
     
     override func viewDidLoad() {
@@ -31,14 +31,41 @@ class ViewController: UIViewController {
         addAnnotation()
         print("QQ")
     }
+    @IBAction func changeTypeAction(_ sender: Any) {
+        mode = mode + 1
+        switch mode {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .satellite
+        case 2:
+            mapView.mapType = .satelliteFlyover
+        case 3:
+            mapView.mapType = .hybrid
+        case 4:
+            mapView.mapType = .hybridFlyover
+        default:
+            mapView.mapType = .standard
+        }
+    }
 
     func addAnnotation() {
         let ann = MKPointAnnotation()
+        var arr : [CLLocationCoordinate2D] = []
         let location = lm.getLocation() as (Double, Double)
-        
-        ann.coordinate = CLLocationCoordinate2DMake(location.0, location.1)
-        ann.title = lm.getCountry()
-        self.mapView.addAnnotation(ann)
+        arr.append(CLLocationCoordinate2DMake(location.0     , location.1     ))
+        arr.append(CLLocationCoordinate2DMake(location.0+0.01, location.1     ))
+        arr.append(CLLocationCoordinate2DMake(location.0+0.01, location.1+0.01))
+        arr.append(CLLocationCoordinate2DMake(location.0     , location.1+0.01))
+        let poly = MKPolygon(coordinates: arr, count: arr.count)
+        mapView.add(poly)
+//        ann.coordinate = CLLocationCoordinate2DMake(location.0, location.1)
+//        ann.title = lm.getCountry()
+//        self.mapView.addAnnotation(ann)
+    }
+    
+    func act() {
+        print("OWO")
     }
 
 }
@@ -47,16 +74,58 @@ extension ViewController : MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         //
         
-        let view = MKPinAnnotationView.init(annotation: annotation, reuseIdentifier: "Pin")
+        var view : MKPinAnnotationView?
+        var pict : MKAnnotationView?
+        view = mapView.dequeueReusableAnnotationView(withIdentifier: "Pin") as? MKPinAnnotationView
+        pict = mapView.dequeueReusableAnnotationView(withIdentifier: "Pin")
         
-        if (annotation.title)!! as String == "Taiwan" {
-            view.pinTintColor = .green
-//            print(view)
-//            print("@@")
+        if view == nil {
+            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "Pin")
         }
-        view.canShowCallout = true
-        
+        if (annotation.title)!! as String == "Taiwan" {
+            view?.pinTintColor = .green
+            let imgView = UIImageView(image: UIImage(named: "a.jpg"))
+            view?.leftCalloutAccessoryView = imgView
+            let btn = UIButton(type: .detailDisclosure)
+            btn.tag = 0
+            btn.addTarget(self, action: #selector(act), for: .touchUpInside)
+            view?.rightCalloutAccessoryView = btn
+            
+        }
+        view?.isDraggable    = true
+        view?.canShowCallout = true
         return view
+
+        
+//        if pict == nil {
+//            pict = MKAnnotationView(annotation: annotation, reuseIdentifier: "Pin")
+//            print("!!")
+//        }
+//        pict?.image = UIImage(named: "a.jpg")
+//        pict?.isDraggable = true
+//        pict?.canShowCallout = true
+//        return pict
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, didChange newState: MKAnnotationViewDragState, fromOldState oldState: MKAnnotationViewDragState) {
+        if newState == .ending {
+            view.dragState = .none
+            print("WW")
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        //
+        
+        print("select now")
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let render = MKPolygonRenderer(overlay: overlay)
+        render.fillColor = UIColor.green.withAlphaComponent(0.5)
+        render.strokeColor = (.blue)
+        render.lineWidth = 5.0
+        return render
     }
 }
 
